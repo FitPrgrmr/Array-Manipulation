93f49ad46a22c5bd8a8e8c18aa63649ee0d5b989
Jymax4real <94472817+Jymax4real@users.noreply.github.com>
2022-05-12 23:17:48 +0200

    Manipulation of Multi-dimensional Arrays

--- ArrayDemo/src/Bi_dimensional_Arrays.java
+++ ArrayDemo/src/Bi_dimensional_Arrays.java
@@ -6,11 +6,19 @@ public class Bi_dimensional_Arrays {
 	public static void main(String[] args) {
 		
 		int [][] arr = new int [2][6];//2 elements that are each able to refer to 6 objects
-		//12 total accessible elements
+		//12 total accessible elements//symbolizes arrays referenced by the elements of an outer array
 		
 		arr[0][0] = 45;
 		arr[0][1] = 46;
 		arr[0][5] = 52;
+		
+		
+		
+		arr[0] = arr[1];//the element on idx[0] references to the same obj of element on ind[1]
+		System.out.println(arr[0]+"\t" + arr[1] + "\n I am a genius! haha\n\n");
+		
+		
+		
 		System.out.println(" # of references that can access "+ arr[0].length+" other elements is\n"+ arr.length+" \nlength of each inner array " + arr[0].length);
 		System.out.println("array's length is: " + arr.length);
 		int x = 1;
